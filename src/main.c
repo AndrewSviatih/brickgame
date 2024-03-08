9dc561c846ad50ec6e48110617cb4aa47a937daf
sv@DESKTOP-7S6SC45 <svWslWindw>
2024-03-08 23:16:39 +0300

    folders init

--- src/main.c
+++ src/main.c
@@ -1,5 +1,17 @@
 #include "brickgame/tetris/s21_bricks_logic.h"
 
+typedef struct {
+    int a;
+    int b;
+} test;
+
 int main() {
-    ;
+    test testing;
+
+    testing.a = 1;
+    testing.b = 2;
+
+    int res = testing.a + testing.b;
+
+    printf("%d\n", res);
 }
