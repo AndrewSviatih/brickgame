b4d07664a6ae265990c5862a4af79cc725a78d5f
sv@DESKTOP-7S6SC45 <svWslWindw>
2024-03-08 22:18:51 +0300

    folders init

---
+++ src/brickgame/tetris/s21_bricks_logic.h
@@ -0,0 +1,7 @@
+#ifndef STRING_S21_STRING_H
+#define STRING_S21_STRING_H
+
+#include "stdio.h"
+#include "stdlib.h"
+
+#endif  // STRING_S21_STRING_H
---
+++ src/tests/s21_tests.h
@@ -0,0 +1,15 @@
+#ifndef SRC_TESTS_INCLUDES_S21_TESTS_H_
+#define SRC_TESTS_INCLUDES_S21_TESTS_H_
+
+#include <check.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BUFF_SIZE 1000
+
+#include "../brickgame/tetris/s21_bricks_logic.h"
+
+void run_tests(void);
+void run_testcase(Suite *testcase, int counter_testcase);
+
+#endif  // SRC_TESTS_INCLUDES_S21_TESTS_H_
